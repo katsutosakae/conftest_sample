ba653e260e0319baeafd74e1336ce7be5f24bd85
katsutosakae <137148479+katsutosakae@users.noreply.github.com>
2026-02-15 13:34:35 +0900

    軽微な修正

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
-    region = "us-west-1"
+  region                      = "us-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+
+  access_key = "dummy"
+  secret_key = "dummy"
 }
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
-  ami = "ami-09b4b74c"
+  ami           = "ami-09b4b74c"
 }
